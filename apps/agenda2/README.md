1f884e1cfef4de96646b914004a976218aad5491
Flaparoo <629229+flaparoo@users.noreply.github.com>
2023-11-28 17:40:56 +0800

    New app: agenda2

---
+++ apps/agenda2/README.md
@@ -0,0 +1,5 @@
+# Another Agenda App
+
+This app shows a simple list of upcoming events, both from the synced Android calendar as well as alarms/timers/etc.
+
+
---
+++ apps/agenda2/agenda2.app.js
@@ -0,0 +1,182 @@
+/*
+ * Timeline - Banglejs
+ *
+ */
+
+const SHOW_DAYS = 3;
+
+require('DateExt');
+require("Font8x16").add(Graphics); 
+const c = require('colourslib');
+
+const maxStart = Date.now() + SHOW_DAYS*86400000;
+const dateColour = ( g.theme.dark ? 'Green' : 'Blue' );
+
+var allEvents;
+var scrollOffset = 0;
+var maxScrollOffset = 0;
+
+
+function getEvents() {
+  allEvents = [];
+
+  // get all Alarms / Timers / Events
+  var alarms = require("sched").getAlarms();
+  var activeAlarms = require("sched").getActiveAlarms(alarms);
+
+  for (let idx in alarms) {
+    let alarm = alarms[idx];
+
+    let time = require("time_utils").decodeTime(alarm.t);
+    if (! alarm.on) continue;
+
+    let timeToAlarm = require("sched").getTimeToAlarm(alarm);
+    if (! timeToAlarm) continue;
+
+    let ts = new Date(Date.now() + timeToAlarm);
+    if (ts > maxStart) continue;
+
+    let title = 'Alarm';
+    if ("timer" in alarm) title = 'Timer';
+    if ("date" in alarm) title = 'Event';
+
+    allEvents.push({
+      'start': ts,
+      'end': undefined,
+      'title': title,
+      'loc': '',
+      'allday': false
+    });
+  }
+
+  // get all Calendar events
+  var calendar = require("Storage").readJSON("android.calendar.json",true)||[];
+  for (let idx in calendar) {
+    let event = calendar[idx];
+
+    if (event.timestamp*1000 > maxStart) continue;
+
+    let start = new Date(event.timestamp*1000);
+    let end = new Date((event.timestamp + event.durationInSeconds)*1000);
+    let title = event.title;
+    let loc = event.location;
+    let allday = event.allDay;
+    allEvents.push({
+      'start': start,
+      'end': end,
+      'title': title,
+      'loc': loc,
+      'allday': allday
+    });
+  }
+
+  // sort by start timestamp
+  allEvents.sort( (a, b) => a.start.getTime() - b.start.getTime() );
+}
+
+
+function drawEvents() {
+  let y = scrollOffset;
+  let prevDate = '';
+
+  g.clear(reset);
+
+  for (let idx in allEvents) {
+    let event = allEvents[idx];
+
+    let date = event.start.as('T D. C').str;
+
+    // separator
+    if (date != prevDate) {
+      c.setColour(dateColour);
+      if (y > -4 && y < g.getHeight())
+        g.fillRect(0, y, g.getWidth(), y + 2);
+      y += 4;
+    } else {
+      c.setColour('Grey');
+      if (y > -2 && y < g.getHeight())
+        g.drawLine(0, y, g.getWidth(), y);
+      y += 2;
+    }
+
+    // date
+    g.setFont("8x16");
+    if (date != prevDate) {
+      g.setFontAlign(1, -1);
+      if (y > -16 && y < g.getHeight())
+        g.drawString(date, g.getWidth(), y, false);
+      prevDate = date;
+    }
+
+    // time
+    let time = '';
+    if (event.allday) {
+      time = 'All-day';
+    } else {
+      time = event.start.as("h:0m").str;
+      if (event.end) {
+        time += ' - ' + event.end.as("h:0m").str;
+      }
+    }
+    g.setColor(g.theme.fg);
+    g.setFontAlign(-1, -1);
+    if (y > -16 && y < g.getHeight())
+      g.drawString(time, 0, y, false);
+    y += 16;
+
+    // title
+    g.setFontAlign(0, -1).setFont("Vector", 20);
+    let titleLines = g.wrapString(event.title, g.getWidth());
+    let titleHeight = g.stringMetrics(titleLines.join("\n")).height + 1;
+    if (y > titleHeight * -1 && y < g.getHeight())
+      g.drawString(titleLines.join("\n"), g.getWidth()/2, y, false);
+    y += titleHeight;
+
+    // location
+    if (event.loc) {
+      g.setFontAlign(-1, -1).setFont("6x15");
+      let locLines = g.wrapString(event.loc, g.getWidth());
+      let locHeight = g.stringMetrics(locLines.join("\n")).height;
+      if (y > locHeight * -1 && y < g.getHeight())
+        g.drawString(locLines.join("\n"), 0, y, false);
+      y += locHeight;
+    }
+
+    y += 1;
+  }
+
+  maxScrollOffset = (y - scrollOffset - g.getHeight()) * -1;
+}
+
+
+// initialise
+getEvents();
+drawEvents();
+
+// scroll (up/down swipes)
+Bangle.setUI("updown", action => {
+  switch (action) {
+    case -1:  // up
+      if (scrollOffset > maxScrollOffset) {
+        scrollOffset -= 40;
+        if (scrollOffset < maxScrollOffset) { scrollOffset = maxScrollOffset; }
+      }
+      break;
+    case 1:   // down
+      if (scrollOffset < 0) {
+        scrollOffset += 40;
+        if (scrollOffset > 0) { scrollOffset = 0; }
+      }
+      break;
+    default:
+      // ignore taps
+  }
+  drawEvents();
+});
+// exit on button press
+setWatch(e => { Bangle.showClock(); }, BTN1);
+
+
+
+// for debugging:
+//console.log(allEvents);
