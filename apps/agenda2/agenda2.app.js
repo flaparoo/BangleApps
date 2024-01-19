e4520e141029aba5dbbb85c9ac6426d3b509abb3
Flaparoo <629229+flaparoo@users.noreply.github.com>
2024-01-19 11:31:14 +0800

    agenda2: refactored scolling (to use E.showScroller)

--- apps/agenda2/agenda2.app.js
+++ apps/agenda2/agenda2.app.js
@@ -21,8 +21,7 @@ const sunColour = ( g.theme.dark ? COLOUR_YELLOW : COLOUR_DARK_YELLOW );
 
 var allEvents;
 var sunTimes = {};
-var scrollOffset = 0;
-var maxScrollOffset = 0;
+var lines = [];
 
 
 function getEvents() {
@@ -84,127 +83,122 @@ function getEvents() {
 }
 
 
-function drawEvents() {
-  g.clear(true);
+function buildLines() {
+  // first line is sunrise/sunset info
+  lines.push({ t: 'sol' });
 
-  let y = 0;
-  let prevDate = '';
-  let horizontalCenter = g.getWidth() / 2;
-
-  // sunrise and sunset info
-  if (! scrollOffset) {
-    g.setColor(sunColour);
-    g.setFont("8x16");
-    if ('sunrise' in sunTimes) {
-      g.setFontAlign(-1, -1);
-      g.drawString(require("locale").time(sunTimes.sunrise, 1).trim(), 0, y, false);
-      g.setFontAlign(0, -1);
-      g.drawString('< Sun >', horizontalCenter, y, false);
-      g.setFontAlign(1, -1);
-      g.drawString(require("locale").time(sunTimes.sunset, 1), g.getWidth(), y, false);
-    } else {
-      g.setFontAlign(0, -1);
-      g.drawString('*"My Location" not set*', horizontalCenter, y, false);
-    }
-    y += 16;
-  }
-
+  // in case there are no upcoming events
   if (! allEvents.length) {
-    g.setColor(dateColour).setFontAlign(0, 0).setFont("Vector", 20);
-    let msg = g.wrapString('You can relax - there are no upcoming events!', g.getWidth());
-    g.drawString(msg.join("\n"), horizontalCenter, g.getHeight() / 2, false);
+    lines.push({ t: 'empty' });
+    lines.push({ t: 'empty' });
+    lines.push({ t: 'title', title: 'You can relax!' });
+    lines.push({ t: 'title', title: '-' });
+    lines.push({ t: 'title', title: 'There are no' });
+    lines.push({ t: 'title', title: 'upcoming events' });
     return;
   }
 
-  maxScrollOffset = allEvents.length - 1;
-
-  // up arrow (if not showing first event)
-  if (scrollOffset) {
-    g.setColor(dateColour);
-    g.fillRect(0, 0, g.getWidth(), 7);
-    g.setColor(g.theme.bg);
-    g.fillPoly([ horizontalCenter, 0,
-                 horizontalCenter + 7, 7,
-                 horizontalCenter - 7, 7 ]);
-    y += 8;
-  }
-
-  for (let idx = scrollOffset; idx < allEvents.length; idx++) {
-    let event = allEvents[idx];
-
+  // loop all events
+  let prevDate = '';
+  for (let event of allEvents) {
     let date = require("locale").dow(event.start, 1) + ' ' +
                event.start.getDate() + '. ' + require("locale").month(event.start, 1);
-
-    // separator
-    if (date != prevDate) {
-      g.setColor(dateColour);
-      g.fillRect(0, y, g.getWidth(), y + 2);
-      y += 4;
+    let time;
+    if (event.allday) {
+      time = 'All-day';
     } else {
-      g.setColor(COLOUR_GREY);
-      g.drawLine(0, y, g.getWidth(), y);
-      y += 2;
+      time = require("locale").time(event.start, 1);
+      if (event.end) {
+        time += ' - ' + require("locale").time(event.end, 1);
+      }
     }
 
-    if (y < eventAreaHeight) {
-      // date
-      g.setFont("8x16");
-      if (date != prevDate) {
-        g.setFontAlign(-1, -1);
-        g.drawString(date, 0, y, false);
-        prevDate = date;
-      }
+    // date + time line
+    lines.push({ t: 'datetime', date: (date != prevDate ? date : ''), time: time });
+    prevDate = date;
 
-      // time
-      let time = '';
-      if (event.allday) {
-        time = 'All-day';
-      } else {
-        time = require("locale").time(event.start, 1);
-        if (event.end) {
-          time += ' - ' + require("locale").time(event.end, 1);
-        }
-      }
-      g.setColor(g.theme.fg);
-      g.setFontAlign(1, -1);
-      g.drawString(time, g.getWidth(), y, false);
-
-      y += 16;
-    }
-
-    // title
-    if (y < eventAreaHeight) {
-      g.setFontAlign(0, -1).setFont("Vector", 20);
-      let titleLines = g.wrapString(event.title, g.getWidth());
-      let titleHeight = g.stringMetrics(titleLines.join("\n")).height + 1;
-      g.drawString(titleLines.join("\n"), horizontalCenter, y, false);
-      y += titleHeight;
+    // title lines
+    g.setFont("Vector", 20);
+    let titleLines = g.wrapString(event.title, g.getWidth());
+    for (let line of titleLines) {
+      lines.push({ t: 'title', title: line });
     }
 
     // location
-    if (event.loc && y < eventAreaHeight) {
-      g.setFontAlign(0, -1).setFont("6x15");
-      let locLines = g.wrapString(event.loc, g.getWidth());
-      let locHeight = g.stringMetrics(locLines.join("\n")).height;
-      g.drawString(locLines.join("\n"), horizontalCenter, y, false);
-      y += locHeight;
+    if (event.loc) {
+      g.setFont("6x15");
+      var locWidth = g.stringMetrics(event.loc).width;
+      var clipped = false;
+      while (locWidth > (g.getWidth() - 7)) {
+        event.loc = event.loc.slice(0, -1);
+        locWidth = g.stringMetrics(event.loc).width;
+        clipped = true;
+      }
+      if (clipped)
+        event.loc += '...';
+      lines.push({ t: 'loc', loc: event.loc });
     }
-
-    if (y >= eventAreaHeight) {
-      // more events than fit on screen -> show down arrow
-      g.setColor(dateColour);
-      g.fillRect(0, g.getHeight() - 10, g.getWidth(), g.getHeight());
-      g.setColor(g.theme.bg);
-      g.fillPoly([ horizontalCenter, g.getHeight() - 1,
-                   horizontalCenter + 7, g.getHeight() - 7,
-                   horizontalCenter - 7, g.getHeight() - 7 ]);
-      break;
-    }
-
-    y += 1;
   }
-  if (y < eventAreaHeight)
-    maxScrollOffset = scrollOffset;
+}
+
+
+function drawLine(idx, rect) {
+  let line = lines[idx];
+  let horizontalCenter = rect.x + (rect.w / 2);
+  let rightAlign = rect.x + rect.w;
+
+  switch (line.t) {
+    case 'empty':
+      break;
+
+    case 'sol':
+      // sunrise and sunset info
+      g.setColor(sunColour).setFont("8x16");
+      if ('sunrise' in sunTimes) {
+        g.setFontAlign(-1, -1);
+        g.drawString(require("locale").time(sunTimes.sunrise, 1).trim(), rect.x, rect.y, false);
+        g.setFontAlign(0, -1);
+        g.drawString('< Daylight >', horizontalCenter, rect.y + 2, false);
+        g.setFontAlign(1, -1);
+        g.drawString(require("locale").time(sunTimes.sunset, 1), rightAlign, rect.y + 4, false);
+      } else {
+        g.setFontAlign(0, -1);
+        g.drawString('*"My Location" not set*', horizontalCenter, rect.y + 2, false);
+      }
+      break;
+
+    case 'datetime':
+      g.setFont("8x16");
+
+      // separator line
+      if (line.date) {
+        g.setColor(dateColour).fillRect(rect.x, rect.y, rightAlign, rect.y + 2);
+        // also print date
+        g.setFontAlign(-1, -1).drawString(line.date, rect.x, rect.y + 4, false);
+      } else {
+        g.setColor(COLOUR_GREY).drawLine(rect.x, rect.y + 2, rightAlign, rect.y + 2);
+      }
+
+      // time
+      g.setColor(g.theme.fg).setFontAlign(1, -1);
+      g.drawString(line.time, rightAlign, rect.y + 4, false);
+      break;
+
+    case 'title':
+      // title line
+      g.setColor(g.theme.fg).setFontAlign(0, -1).setFont("Vector", 20);
+      g.drawString(line.title, horizontalCenter, rect.y, false);
+      break;
+
+    case 'loc':
+      // location
+      g.setColor(g.theme.fg).setFontAlign(0, -1).setFont("6x15");
+      g.drawString(line.loc, horizontalCenter, rect.y + 2, false);
+      break;
+
+    default:
+      console.log('Unknown line type: '+line.t);
+  }
 }
 
 
@@ -215,26 +209,22 @@ if ('lat' in mylocation && 'lon' in mylocation) {
   sunTimes = require("suncalc").getTimes(now, mylocation.lat, mylocation.lon);
 }
 getEvents();
-drawEvents();
+buildLines();
 
-// scroll (up/down taps)
-Bangle.on('touch', (button, xy) => {
-  if (xy.y >= g.getHeight() / 2) {
-    // scroll down
-    scrollOffset++;
-    if (scrollOffset > maxScrollOffset) scrollOffset = maxScrollOffset;
-  } else {
-    // scroll up
-    scrollOffset--;
-    if (scrollOffset < 0) scrollOffset = 0;
-  }
-  drawEvents();
+// show events in scroller
+E.showScroller({
+  h: 20,
+  c: lines.length,
+  draw: drawLine,
+  select: (idx, touch) => {
+    // do nothing for any tap
+  },
+  //back: () => load()
 });
+
 // exit on button press
 setWatch(e => { Bangle.showClock(); }, BTN1);
 
 
-
 // for debugging:
 //console.log(allEvents);
-
